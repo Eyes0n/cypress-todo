2f53b3af99675a1daf131c9498c9501276b1009e
hyunchanpark <eye3570@gmail.com>
2021-09-10 19:23:59 +0900

    feat : Form Submit API (todo 생성)  TEST case 추가

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -2,13 +2,52 @@ describe('Input form', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   it('focuses input on load', () => {
     cy.focused().should('have.class', 'new-todo');
   });
 
-  it.only('accepts input', () => {
+  it('accepts input', () => {
     const typedText = 'Buy Milk';
 
     cy.get('.new-todo').type(typedText).should('have.value', typedText);
+  });
+
+  context('Form submission', () => {
+    beforeEach(() => {
+      cy.server();
+    });
+
+    it('Add a new todo on submit', () => {
+      const itemText = 'Buy eggs';
+      // 해당 api를 가진 서버가 없어도 cypress에서 아래와 같이 서버를 임의로 정의해줘서 사용가능하다.
+      cy.route('POST', 'api/todos', {
+        name: itemText,
+        id: 1,
+        isCompleted: false,
+      });
+
+      cy.get('.new-todo')
+        .type(itemText)
+        .type('{enter}')
+        .should('have.value', '');
+
+      cy.get('.todo-list li').should('have.length', 1).and('contain', itemText);
+    });
+
+    it('Shows an error mesage on a failed submission', () => {
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {},
+      });
+
+      cy.get('.new-todo').type('test{enter}');
+
+      cy.get('.todo-list li').should('not.exist');
+
+      cy.get('.error').should('be.visible');
+    });
   });
 });
