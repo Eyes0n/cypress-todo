8bd0d9c168752189b623da266a8c917a19c4a5e7
hyunchanpark <eye3570@gmail.com>
2021-09-13 19:02:50 +0900

    feat : todo 삭제 TEST case 추가

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Footer from './Footer';
-import { saveTodo, loadTodos } from '../lib/service';
+import { saveTodo, loadTodos, deleteTodo } from '../lib/service';
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -15,6 +15,7 @@ export default class TodoApp extends Component {
     };
     this.handleNewTodoChange = this.handleNewTodoChange.bind(this);
     this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
@@ -29,6 +30,14 @@ export default class TodoApp extends Component {
 
   handleNewTodoChange(evt) {
     this.setState({ currentTodo: evt.target.value });
+  }
+
+  handleDelete(id) {
+    deleteTodo(id).then(() =>
+      this.setState({
+        todos: this.state.todos.filter((t) => t.id !== id),
+      })
+    );
   }
 
   handleTodoSubmit(evt) {
@@ -61,7 +70,10 @@ export default class TodoApp extends Component {
             />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos} />
+            <TodoList
+              todos={this.state.todos}
+              handleDelete={this.handleDelete}
+            />
           </section>
           <Footer remaining={remaining} />
         </div>
