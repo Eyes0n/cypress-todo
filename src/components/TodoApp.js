18108d1746511a66129c3c2a3a3827284920c7b9
hyunchanpark <eye3570@gmail.com>
2021-09-13 16:17:15 +0900

    feat : show remaining todo counts TEST case

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -45,6 +45,9 @@ export default class TodoApp extends Component {
   }
 
   render() {
+    const remaining = this.state.todos.filter(
+      (todo) => !todo.isComplete
+    ).length;
     return (
       <Router>
         <div>
@@ -60,7 +63,7 @@ export default class TodoApp extends Component {
           <section className="main">
             <TodoList todos={this.state.todos} />
           </section>
-          <Footer />
+          <Footer remaining={remaining} />
         </div>
       </Router>
     );
